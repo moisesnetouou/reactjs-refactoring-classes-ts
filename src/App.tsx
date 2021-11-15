4a41a8b92319987424f682ae8af4e8efa00c188d
moises.neto <moisesnetored@gmail.com>
2021-11-14 22:58:00 -0400

    feat: TypeScript adicionado

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Modal } from "./components/Modal";
+import { ModalContext } from "./context/ModalContext";
+
+import { Routes } from "./routes";
+import GlobalStyle from "./styles/global";
+
+function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function handleCloseModal() {
+    setIsModalOpen(false);
+  }
+
+  return (
+    <ModalContext.Provider value={[]}>
+      <GlobalStyle />
+      <Router>
+        <Modal isOpen={isModalOpen} setIsOpen={handleCloseModal} />
+        <Routes />
+      </Router>
+    </ModalContext.Provider>
+  );
+}
+
+export default App;
